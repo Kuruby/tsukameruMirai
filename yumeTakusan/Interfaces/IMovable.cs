3b6189f4d272804c04620d29d89605c50baef1e5
黒澤ルビィ <43500169+Kuruby@users.noreply.github.com>
2018-10-17 20:11:39 -0400

    interfaces for some game object types

---
+++ yumeTakusan/Interfaces/IMovable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace yumeTakusan.Interfaces
+{
+    public interface IMovable :IWorldExistence
+    {
+        Vector2 Velocity { get; set; }
+        float VelocityXComponent { get; set; }
+        float VelocityYComponent { get; set; }
+        Vector2 Acceleration { get; set; }
+        float AccelerationXComponent { get; set; }
+        float AccelerationYComponent { get; set; }
+    }
+}
---
+++ yumeTakusan/Interfaces/IWorldExistence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace yumeUI
+{
+    /// <summary>
+    /// IWorldExistence provides an interface that guarantees that an object will be able to exist in the world.
+    /// </summary>
+    interface IWorldExistense
+    {
+        Vector2 Location { get; set; }
+        float LocationXComponent { get; set; }
+        float LocationYComponent { get; set; }
+    }
+}
