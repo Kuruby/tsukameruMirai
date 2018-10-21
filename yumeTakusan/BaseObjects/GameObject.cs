5704858cd2d211f673c2ecb3e340e833be43142a
黒澤ルビィ <43500169+Kuruby@users.noreply.github.com>
2018-10-21 19:37:37 -0400

    more interfaces and a gameobject that implements them

---
+++ yumeTakusan/BaseObjects/GameObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using yumeTakusan.Interfaces;
+
+namespace yumeTakusan.BaseObjects
+{
+    abstract class GameObject : IWorldExistence, IMovable
+    {
+
+        private Vector2 location;
+        public Vector2 Location
+        {
+            get => location;
+            set { location = value; }
+        }
+        public float LocationXComponent
+        {
+            get => location.X;
+            set { location.X = value; }
+        }
+        public float LocationYComponent
+        {
+            get => location.Y;
+            set { location.Y = value; }
+        }
+
+        private Vector2 velocity = Vector2.Zero;
+        public Vector2 Velocity
+        {
+            get => velocity;
+            set { velocity = value; }
+        }
+        public float VelocityXComponent
+        {
+            get => velocity.X;
+            set { velocity.X = value; }
+        }
+        public float VelocityYComponent
+        {
+            get => velocity.Y;
+            set { velocity.Y = value; }
+        }
+
+        private Vector2 acceleration = Vector2.Zero;
+        public Vector2 Acceleration
+        {
+            get => acceleration;
+            set { acceleration = value; }
+        }
+        public float AccelerationXComponent
+        {
+            get => acceleration.X;
+            set { acceleration.X = value; }
+        }
+        public float AccelerationYComponent
+        {
+            get => acceleration.Y;
+            set { acceleration.Y = value; }
+        }
+
+
+    }
+}
