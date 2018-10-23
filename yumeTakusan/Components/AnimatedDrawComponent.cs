4bdc4aaf537380f3bc306ab7269d1da7356da2f8
黒澤ルビィ <43500169+Kuruby@users.noreply.github.com>
2018-10-23 11:34:03 -0400

    Implement AnimatedDrawComponent

--- yumeTakusan/Components/AnimatedDrawComponent.cs
+++ yumeTakusan/Components/AnimatedDrawComponent.cs
@@ -6,19 +6,41 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using yumeTakusan.Camera;
+using yumeTakusan.Interfaces;
 
 namespace yumeTakusan.Components
 {
     public class AnimatedDrawComponent
     {
-        public void updateFrameRectangle()
+        public AnimatedDrawComponent() : this(null)
         {
 
         }
-
-        public void Draw(SpriteBatch spriteBatch, Camera.Camera camera, GameTime gameTime)
+        public AnimatedDrawComponent(IAnimatable DrawnObject)
         {
+            drawnObject = DrawnObject;
+        }
 
+        IAnimatable drawnObject;
+
+        public IAnimatable DrawnObject
+        {
+            set
+            {
+                drawnObject = value;
+            }
+        }
+
+        public void UpdateFrameRectangle()
+        {
+            drawnObject.frameLocation = new Rectangle(drawnObject.horizontalFrameNumber * drawnObject.frameWidth,
+                drawnObject.verticalFrameNumber * drawnObject.frameHeight,
+                drawnObject.frameWidth, drawnObject.frameHeight);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Camera.Camera camera, GameTime gameTime, float layerDepth)
+        {
+            spriteBatch.Draw(drawnObject.Texture, drawnObject.Location, drawnObject.frameLocation, drawnObject.Color, 0f, Vector2.Zero, 1f, drawnObject.Effects, layerDepth);
         }
 
     }
