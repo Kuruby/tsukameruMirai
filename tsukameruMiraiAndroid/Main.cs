836db8d0ec99696074a3cf4d4b9046d995f7d7c3
黒澤ルビィ <43500169+Kuruby@users.noreply.github.com>
2018-11-09 08:12:19 +0900

    Revert "remove android version due to desktopness in the core content manager"
    This reverts commit f21dfa8333919bcf847a33e5b9c407062f53bc26.

---
+++ tsukameruMiraiAndroid/Main.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using tsukameruCore;
+
+namespace tsukameruMiraiAndroid
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Main : TsukameruGame
+    {
+
+        public Main():base()
+        {
+            
+            graphics.IsFullScreen = true;
+            //Do not set graphics size; tsukameruGame autodetects and will be able to adapt based on this
+            graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+        }
+
+    }
+}
